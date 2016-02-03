b95db101557e537e3fc4861663aeb2a4ef8053da
kawasakimsh <kawasakimsh@nttdata.co.jp>
2016-02-03 12:58:46 +0900

    add jp

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,3 @@
-console.log('This would be the main JS file.');
+if ((window.navigator.userLanguage || window.navigator.language || window.navigator.browserLanguage).substr(0,2) == "ja") {
+  location.href = "../index_ja.html";
+}
